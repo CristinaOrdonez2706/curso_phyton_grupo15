29cb58458fd80881bb142e59925b6e39cf694471
María Cristina Ordoñez Mutiz <mcordonezm@unal.edu.co>
2023-10-29 10:08:25 -0500

    modificación ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,9 +1,12 @@
 #Programa para para validación de númeor par e impar.
 
-numero = int(input("Ingrese un número: "))
-
-
-if numero % 2 == 0:
-    print(f"{numero} es un número par.")
-else:
-    print(f"{numero} es un número impar.")
+try:
+    numero = input("Ingresa un número: ")
+    numero = int(numero)
+    
+    if numero % 2 == 0:
+        print(f"{numero} es un número par.")
+    else:
+        print(f"{numero} es un número impar.")
+except ValueError:
+    print("Error: Ingresa un número válido.")
